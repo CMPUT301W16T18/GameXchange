317fc7301c641d38803e8d7d8d724bf7eb93aa43
Adam Cawthorn <adamtcawt@gmail.com>
2016-03-30 17:41:54 -0600

    added status to bid object for accepting/declining bids.

--- src/GameXchange/app/src/main/java/ca/ualberta/cmput301w16t18/gamexchange/Bid.java
+++ src/GameXchange/app/src/main/java/ca/ualberta/cmput301w16t18/gamexchange/Bid.java
@@ -10,11 +10,13 @@ public class Bid {
     private String bidder;
     private double price;
     private LatLng location;
+    private boolean accepted;
 
     public Bid(String bidder, double price, LatLng location) {
         this.bidder = bidder;
         this.price = price;
         this.location = location;
+        this.accepted = false;
     }
 
     public String getBidder() {
@@ -44,5 +46,13 @@ public class Bid {
     public String getMapsString() {
         // from http://stackoverflow.com/questions/3990110/how-to-show-marker-in-maps-launched-by-geo-uri-intent
         return "geo:" + location.latitude + "," + location.longitude + "?z=10&q=" + location.latitude + "," + location.longitude + "(Trade location)";
+    }
+
+    public boolean getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
     }
 }
